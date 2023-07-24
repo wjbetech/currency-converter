4d012262a29c0ad39a6083a49ea19e8e98a1b1d4
wjbetech <wjbetech@gmail.com>
2023-07-24 14:56:43 +0900

    App finished

--- index.js
+++ index.js
@@ -1,28 +1,72 @@
-import API_KEY from "./apikey.js";
-
 // theme toggle
+
 const toggler = document.getElementById('toggle');
 
 function toggleTheme() {
+    const selectInputs = document.querySelectorAll(".select-input");
+    const selectElements = document.querySelectorAll("select");
+    const inputElements = document.querySelectorAll("input");
+    const convertBoxes = document.querySelectorAll(".convert-box");
+    const reverseElement = document.querySelector(".reverse");
+    const form = document.getElementById('form');
+
+    form.classList.toggle('dark-mode');
     document.body.classList.toggle('dark-mode');
+    toggler.classList.toggle('dark-mode');
+
+    // Toggle dark-mode class on the .nav element
+    const navBar = document.querySelector(".nav");
+    navBar.classList.toggle('dark-mode');
+
+    // Toggle dark-mode class on select-input elements
+    for (const selectInput of selectInputs) {
+        selectInput.classList.toggle('dark-mode');
+    }
+
+    // Toggle dark-mode class on select elements
+    for (const selectElement of selectElements) {
+        selectElement.classList.toggle('dark-mode');
+    }
+
+    // Toggle dark-theme class on input elements
+    for (const inputElement of inputElements) {
+        inputElement.classList.toggle('dark-theme');
+    }
+
+    // Toggle dark-theme class on all .convert-box .select-input
+    for (const convertBox of convertBoxes) {
+        convertBox.querySelector(".select-input").classList.toggle('dark-theme');
+    }
+
+    // Toggle dark-theme class on .reverse element
+    reverseElement.classList.toggle('dark-theme');
+
+    // Save the current theme preference in local storage
     const setTheme = document.body.classList.contains('dark-mode');
     localStorage.setItem('dark-mode', setTheme);
 }
 
+window.addEventListener('DOMContentLoaded', () => {
+    const currentTheme = localStorage.getItem('dark-mode') === 'true';
+    if (currentTheme) {
+        toggleTheme();
+    }
+});
+
 toggler.addEventListener('click', toggleTheme);
 
-console.log(API_KEY);
-
 // currency scripting
-const fromInput = document.querySelector(".from select");
-const toInput = document.querySelector(".to select");
-const getRateButton = document.querySelector("form button");
-const exchangeIcon = document.querySelector("form .reverse");
-const amount = document.querySelector("form input");
-const exchangeRateText = document.querySelector("form .result");
 
-// function to pull country and curr codes
-[fromInput, toInput].forEach((select, i) => {
+const fromCur = document.querySelector(".from select");
+const toCur = document.querySelector(".to select");
+const getBtn = document.querySelector("form button");
+const exIcon = document.querySelector("form .reverse");
+const amount = document.querySelector("form input");
+const exRateTxt = document.querySelector("form .result");
+
+// Event listener for currency dropdowns (select)
+
+[fromCur, toCur].forEach((select, i) => {
     for (let curCode in Country_List) {
         const selected = (i === 0 && curCode === "USD") || (i === 1 && curCode === "GBP") ? "selected" : "";
         select.insertAdjacentHTML("beforeend", `<option value="${curCode}" ${selected}>${curCode}</option>`);
@@ -34,24 +78,39 @@ const exchangeRateText = document.querySelector("form .result");
     });
 });
 
-// function to get exchange rates
+// Function to get exchange rate from api
+
 async function getExchangeRate() {
-    const value = amount.value || 1;
-    exchangeRateText.innerText = "Getting exchange rate...";
+    const amountVal = amount.value || 1;
+    exRateTxt.innerText = "Getting exchange rate...";
     try {
-        const response = await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${fromInput.value}`);
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/ede0f1ee2944e42c8bdc9c1b/latest/${fromCur.value}`);
         const result = await response.json();
-        const exchangeRate = result.conversion.rates[toInput.value];
-        const totalExchangeRate = (amount * exchangeRate).toFixed(2);
-        exchangeRateText.innerText = `${value} ${fromInput.value} = ${totalExchangeRate} ${toInput.value}`;
+        const exchangeRate = result.conversion_rates[toCur.value];
+        const totalExRate = (amountVal * exchangeRate).toFixed(2);
+        exRateTxt.innerText = `${amountVal} ${fromCur.value} = ${totalExRate} ${toCur.value}`;
     } catch (error) {
-        console.log("ERROR - Failed to find correct exchange rate data")
-        exchangeRateText.innerText = "Oh no! Something went wrong...";
+        exRateTxt.innerText = "Something went wrong...";
     }
 }
 
-// Event handlers for buttons and icons
-window.addEventListener("load", getExchangeRate)
+// Event listeners for button and exchange icon click
+
+window.addEventListener("load", getExchangeRate);
+getBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getExchangeRate();
+});
+
+exIcon.addEventListener("click", () => {
+    [fromCur.value, toCur.value] = [toCur.value, fromCur.value];
+    [fromCur, toCur].forEach((select) => {
+        const code = select.value;
+        const imgTag = select.parentElement.querySelector("img");
+        imgTag.src = `https://flagcdn.com/48x36/${Country_List[code].toLowerCase()}.png`;
+    });
+    getExchangeRate();
+});
 
 
 
